81c9b0bb5bcdef66f79694070d2204b763c58530
GeorgeSecker <116958@newporthigh.co.uk>
2017-03-29 13:58:55 +0100

    Add files via upload

---
+++ VB/VB/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFName = New System.Windows.Forms.TextBox()
+        Me.txtLName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.dtpDOB = New System.Windows.Forms.DateTimePicker()
+        Me.cmdDisplay = New System.Windows.Forms.Button()
+        Me.txtSave = New System.Windows.Forms.RichTextBox()
+        Me.lblFirst = New System.Windows.Forms.Label()
+        Me.lblLast = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.cmdDelete = New System.Windows.Forms.Button()
+        Me.cmdLoad = New System.Windows.Forms.Button()
+        Me.cmdReset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFName
+        '
+        Me.txtFName.Location = New System.Drawing.Point(84, 21)
+        Me.txtFName.Name = "txtFName"
+        Me.txtFName.Size = New System.Drawing.Size(100, 20)
+        Me.txtFName.TabIndex = 0
+        '
+        'txtLName
+        '
+        Me.txtLName.Location = New System.Drawing.Point(84, 47)
+        Me.txtLName.Name = "txtLName"
+        Me.txtLName.Size = New System.Drawing.Size(100, 20)
+        Me.txtLName.TabIndex = 1
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(84, 73)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(100, 20)
+        Me.txtEmail.TabIndex = 2
+        '
+        'dtpDOB
+        '
+        Me.dtpDOB.Location = New System.Drawing.Point(84, 99)
+        Me.dtpDOB.Name = "dtpDOB"
+        Me.dtpDOB.Size = New System.Drawing.Size(200, 20)
+        Me.dtpDOB.TabIndex = 5
+        Me.dtpDOB.Value = New Date(2017, 3, 29, 0, 0, 0, 0)
+        '
+        'cmdDisplay
+        '
+        Me.cmdDisplay.Location = New System.Drawing.Point(96, 151)
+        Me.cmdDisplay.Name = "cmdDisplay"
+        Me.cmdDisplay.Size = New System.Drawing.Size(75, 23)
+        Me.cmdDisplay.TabIndex = 6
+        Me.cmdDisplay.Text = "Check"
+        Me.cmdDisplay.UseVisualStyleBackColor = True
+        '
+        'txtSave
+        '
+        Me.txtSave.Location = New System.Drawing.Point(324, 12)
+        Me.txtSave.Name = "txtSave"
+        Me.txtSave.Size = New System.Drawing.Size(409, 227)
+        Me.txtSave.TabIndex = 7
+        Me.txtSave.Text = ""
+        '
+        'lblFirst
+        '
+        Me.lblFirst.AutoSize = True
+        Me.lblFirst.Location = New System.Drawing.Point(24, 21)
+        Me.lblFirst.Name = "lblFirst"
+        Me.lblFirst.Size = New System.Drawing.Size(60, 13)
+        Me.lblFirst.TabIndex = 8
+        Me.lblFirst.Text = "First Name:"
+        '
+        'lblLast
+        '
+        Me.lblLast.AutoSize = True
+        Me.lblLast.Location = New System.Drawing.Point(24, 47)
+        Me.lblLast.Name = "lblLast"
+        Me.lblLast.Size = New System.Drawing.Size(52, 13)
+        Me.lblLast.TabIndex = 9
+        Me.lblLast.Text = "Surname:"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(24, 76)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(35, 13)
+        Me.lblEmail.TabIndex = 10
+        Me.lblEmail.Text = "Email:"
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Location = New System.Drawing.Point(7, 99)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(71, 13)
+        Me.lblDOB.TabIndex = 11
+        Me.lblDOB.Text = "Date Of Birth:"
+        '
+        'cmdSave
+        '
+        Me.cmdSave.Location = New System.Drawing.Point(96, 180)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSave.TabIndex = 13
+        Me.cmdSave.Text = "Save"
+        Me.cmdSave.UseVisualStyleBackColor = True
+        '
+        'cmdDelete
+        '
+        Me.cmdDelete.Location = New System.Drawing.Point(177, 165)
+        Me.cmdDelete.Name = "cmdDelete"
+        Me.cmdDelete.Size = New System.Drawing.Size(75, 23)
+        Me.cmdDelete.TabIndex = 14
+        Me.cmdDelete.Text = "Delete Check"
+        Me.cmdDelete.UseVisualStyleBackColor = True
+        '
+        'cmdLoad
+        '
+        Me.cmdLoad.Location = New System.Drawing.Point(96, 220)
+        Me.cmdLoad.Name = "cmdLoad"
+        Me.cmdLoad.Size = New System.Drawing.Size(75, 30)
+        Me.cmdLoad.TabIndex = 15
+        Me.cmdLoad.Text = "Load"
+        Me.cmdLoad.UseVisualStyleBackColor = True
+        '
+        'cmdReset
+        '
+        Me.cmdReset.Location = New System.Drawing.Point(177, 224)
+        Me.cmdReset.Name = "cmdReset"
+        Me.cmdReset.Size = New System.Drawing.Size(75, 23)
+        Me.cmdReset.TabIndex = 16
+        Me.cmdReset.Text = "Reset"
+        Me.cmdReset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(745, 262)
+        Me.Controls.Add(Me.cmdReset)
+        Me.Controls.Add(Me.cmdLoad)
+        Me.Controls.Add(Me.cmdDelete)
+        Me.Controls.Add(Me.cmdSave)
+        Me.Controls.Add(Me.lblDOB)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblLast)
+        Me.Controls.Add(Me.lblFirst)
+        Me.Controls.Add(Me.txtSave)
+        Me.Controls.Add(Me.cmdDisplay)
+        Me.Controls.Add(Me.dtpDOB)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtLName)
+        Me.Controls.Add(Me.txtFName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtFName As System.Windows.Forms.TextBox
+    Friend WithEvents txtLName As System.Windows.Forms.TextBox
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents dtpDOB As System.Windows.Forms.DateTimePicker
+    Friend WithEvents cmdDisplay As System.Windows.Forms.Button
+    Friend WithEvents txtSave As System.Windows.Forms.RichTextBox
+    Friend WithEvents lblFirst As System.Windows.Forms.Label
+    Friend WithEvents lblLast As System.Windows.Forms.Label
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
+    Friend WithEvents lblDOB As System.Windows.Forms.Label
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents cmdDelete As System.Windows.Forms.Button
+    Friend WithEvents cmdLoad As System.Windows.Forms.Button
+    Friend WithEvents cmdReset As System.Windows.Forms.Button
+
+End Class
---
+++ VB/VB/Form1.vb
@@ -0,0 +1,125 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Public Class Form1
+    Dim ClientID As Integer = 0
+
+    Private Sub cmdEnter_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDisplay.Click
+
+        Dim Empty As String = "A textbox has been left empty. Please enter a value now."
+        Dim Title As String = "An Error Has Occured"
+        Dim Button As MessageBoxButtons = MessageBoxButtons.OK  'For use in future message boxes
+
+        Dim PreCheck As Boolean = False
+
+
+
+
+        'Checks for empty textboxes
+
+        If txtFName.Text = "" Then
+            MessageBox.Show(Empty, Title, Button)
+        ElseIf txtLName.Text = "" Then
+            MessageBox.Show(Empty, Title, Button)
+        ElseIf txtEmail.Text = "" Then
+            MessageBox.Show(Empty, Title, Button)
+        Else : PreCheck = True
+        End If
+
+ 
+
+
+
+        If PreCheck = True Then
+            ClientID = ClientID + 1
+            txtSave.AppendText(vbTab & "  Client ID:  " & ClientID & vbNewLine)
+            txtSave.AppendText("" + vbNewLine)
+            txtSave.AppendText(vbTab & lblFirst.Text + "  :  " + txtFName.Text + vbNewLine) 'vbnewline adds a new line
+            txtSave.AppendText("" + vbNewLine)
+            txtSave.AppendText(vbTab & lblLast.Text + "  :  " + txtLName.Text + vbNewLine) 'Will add a tab, corresponding label name and the correct textbox value to the big textbox
+            txtSave.AppendText("" + vbNewLine)
+            txtSave.AppendText(vbTab & lblEmail.Text + "  :  " + txtEmail.Text + vbNewLine)
+            txtSave.AppendText("" + vbNewLine)
+            txtSave.AppendText(vbTab & lblDOB.Text + "  :  " + dtpDOB.Value + vbNewLine) 'This section will add the values of every textbox into a bigger textbox using lines to seperate them to make the txt file easier to read 
+            txtSave.AppendText("" + vbNewLine)
+        End If
+
+
+
+
+    End Sub
+
+
+    Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSave.Click
+        Dim Path As String = "c:\vb\customerdetails.txt"
+        Dim Direct As String = "c:\vb\" 'path that will be used to store the txt file
+
+
+
+        If File.Exists(Path) = True Then
+            Dim Details As String = txtSave.Text
+            File.AppendAllText(Path, Details) ' Will add data stored in the big textbox to the txt file
+
+        Else
+
+            If Not Directory.Exists(Direct) Then
+                Directory.CreateDirectory(Direct) ' This will create the path in the previous comment 
+                Dim TextFile As System.IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter("c:\vb\customerdetails.txt", True) 'will create the textfile as the previous line can only create the directory
+                TextFile.Close() 'prevents crashes 
+                MessageBox.Show("Directory created. Please reclick save to save your data", "", MessageBoxButtons.OK)
+            End If
+        End If
+
+
+
+
+
+
+    End Sub
+
+    Private Sub cmdDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDelete.Click
+        Dim YesNo As MessageBoxButtons = MessageBoxButtons.YesNo
+        Dim Warning As String = "WARNING!"
+        Dim WarnText As String = "You are about to delete your inputted data. Proceed?"
+
+
+        Dim Check As Integer = MessageBox.Show(WarnText, Warning, YesNo) ' Makes sure that the user doesn't accidently delete the data they have inputted
+        If Check = DialogResult.Yes Then
+            txtSave.Text = ""
+
+
+        End If
+
+    End Sub
+
+    Private Sub cmdLoad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdLoad.Click
+
+        txtSave.Text = System.IO.File.ReadAllText("c:\vb\customerdetails.txt") 'Will load the txt file to the textbox
+
+
+        If txtSave.Text = "" Then
+            MessageBox.Show("No Data Was Found", "Error", MessageBoxButtons.OK) 'Will display an error message saying that there is no data in the file
+        End If
+
+    End Sub
+
+    Private Sub cmdReset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdReset.Click
+        Dim Path As String = "c:\vb\customerdetails.txt"
+        Dim Check As Integer = MessageBox.Show("Do you want to erase all stored records?", "WARNING", MessageBoxButtons.YesNo)
+        If Check = DialogResult.Yes Then
+            System.IO.File.WriteAllText(Path, "") 'Will clear the txt file by overwriting using an empty character
+        End If
+
+
+    End Sub
+
+    Private Sub txtFName_KeyPress(ByVal sender As Object, ByVal e As KeyEventArgs) Handles txtFName.KeyDown, txtLName.KeyDown
+
+
+        If (e.KeyCode >= Keys.D0 And e.KeyCode <= Keys.D9) Then ' This will prevent users from typing in numbers and will automatically send a backspace to remove any numbers
+            SendKeys.Send("{BackSpace}")
+        End If
+
+    End Sub
+End Class
+
